0e6a853efeab027169c2fb9f5c9b7bcc35e5af5f
山本　晃大 <cf0293@CYGAMES-1818.local>
2017-10-16 15:03:01 +0900

    modify .gitignore for Quartus

--- src/td4.v
+++ src/td4.v
@@ -1,6 +1,8 @@
-module td4(CLK, CLR);
+module td4(CLK, CLR, Out);
   input CLK, CLR;
+  input [3:0] Out;
   wire CLK, CLR;
+  wire [3:0] Out;
 
-  ARegister areg(CLK, CLR, 1'b1, 1'b1, 4'b0000, 4'b0000);
+  ARegister areg(CLK, CLR, 1'b1, 1'b1, 4'b0000, Out);
 endmodule
