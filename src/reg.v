29be2222b38630ad515a7d4eed22646752c8cb51
山本　晃大 <cf0293@CYGAMES-1818.local>
2017-10-16 16:20:57 +0900

    concat register modules to td4-top

--- src/reg.v
+++ src/reg.v
@@ -1,31 +1,7 @@
 // NOTE: if Im is XXXX when LOAD is Low, Out may be XXXX
 
-/* A register */
-module ARegister(CLK, CLR, EN, LOAD, Im, Out);
-  input CLK, CLR, EN, LOAD;
-  input [3:0] Im;
-
-  wire CLK, CLR, EN, LOAD;
-  wire [3:0] Im;
-
-  output reg [3:0] Out;
-
-  always @ (posedge CLK or negedge CLR) begin
-    if (CLR == 1'b0)
-      Out <= 4'b0000;
-    else
-      if (LOAD == 1'b0)
-        Out <= Im;
-      else
-        Out <= Out;
-  end
-endmodule
-
-
-
-
-/* B register */
-module BRegister(CLK, CLR, EN, LOAD, Im, Out);
+/* General-Purpose register */
+module GPRegister(CLK, CLR, EN, LOAD, Im, Out);
   input CLK, CLR, EN, LOAD;
   input [3:0] Im;
 
--- src/td4.v
+++ src/td4.v
@@ -1,8 +1,26 @@
-module td4(CLK, CLR, Out);
+module td4(CLK, CLR);
   input CLK, CLR;
-  input [3:0] Out;
-  wire CLK, CLR;
-  wire [3:0] Out;
+  // input [3:0] Out;
 
-  ARegister areg(CLK, CLR, 1'b1, 1'b1, 4'b0000, Out);
+  // Register
+  wire CLK, CLR, EN, LOAD0, LOAD1, LOAD2, LOAD3;
+  wire [3:0] Im;
+  wire [3:0] OutA, OutB, OutLED;
+
+  // Rom
+  wire [3:0] Address;
+  wire [7:0] Order;
+
+  // Carry
+  wire Carry, CarryOut;
+
+  // ROM
+  ROM ROM(Address, Order);
+
+  // register
+  GPRegister ARegister(CLK, CLR, EN, LOAD0, Im, OutA);
+  GPRegister BRegister(CLK, CLR, EN, LOAD1, Im, OutB);
+  GPRegister OutPortRegister(CLK, CLR, EN, LOAD2, Im, OutLED);
+  PC PC(CLK, CLR, EN, LOAD3, Im, Address);
+  FlagRegister FlagRegister(CLK, CLR, Carry, CarryOut);
 endmodule
