707590df0274de4528a0b4c8a4b1304c3552e374
山本　晃大 <cf0293@CYGAMES-1818.local>
2017-10-17 15:13:35 +0900

    Modify I/O with outside of CPU

--- src/td4.v
+++ src/td4.v
@@ -1,11 +1,11 @@
-module td4(CLK, CLR);
+module td4(CLK, CLR, IN, OUT);
   input CLK, CLR;
-  // input [3:0] Out;
+  input [3:0] IN, OUT;
 
   // Register
   wire CLK, CLR, EN;
   wire [3:0] LOAD;
-  wire [3:0] OutA, OutB, OutLED;
+  wire [3:0] OutA, OutB;
 
   // Rom
   wire [3:0] Address;
@@ -15,11 +15,10 @@ module td4(CLK, CLR);
   wire [1:0] SELECT;
 
   // Data Selector
-  wire [3:0] InPort;
   wire [3:0] OutDataSelector;
 
   // ALU
-  wire [3:0] OutALU;
+  wire [3:0] ALUResult;
 
   // Carry
   wire Carry, CarryOut;
@@ -31,15 +30,15 @@ module td4(CLK, CLR);
   Decoder Decoder(Order[7:4], CarryOut, LOAD, SELECT);
 
   // Data Selector
-  DataSelector DataSelector(OutA, OutB, InPort, SELECT, OutDataSelector);
+  DataSelector DataSelector(OutA, OutB, IN, SELECT, OutDataSelector);
 
   // ALU
-  ALU ALU(OutDataSelector, Order[3:0], OutALU, Carry);
+  ALU ALU(OutDataSelector, Order[3:0], ALUResult, Carry);
 
   // register
-  GPRegister ARegister(CLK, CLR, EN, LOAD[0], OutALU, OutA);
-  GPRegister BRegister(CLK, CLR, EN, LOAD[1], OutALU, OutB);
-  GPRegister OutPortRegister(CLK, CLR, EN, LOAD[2], OutALU, OutLED);
-  PC PC(CLK, CLR, EN, LOAD[3], OutALU, Address);
+  GPRegister ARegister(CLK, CLR, EN, LOAD[0], ALUResult, OutA);
+  GPRegister BRegister(CLK, CLR, EN, LOAD[1], ALUResult, OutB);
+  GPRegister OutPortRegister(CLK, CLR, EN, LOAD[2], ALUResult, OUT);
+  PC PC(CLK, CLR, EN, LOAD[3], ALUResult, Address);
   FlagRegister FlagRegister(CLK, CLR, Carry, CarryOut);
 endmodule
